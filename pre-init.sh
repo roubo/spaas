9693a60cccdfcbfd7f9a39fa96a5629833bc287a
junjian <junjian_11@163.com>
2019-12-22 16:22:06 +0800

    Feature: 更新pre-init

--- pre-init.sh
+++ pre-init.sh
@@ -3,3 +3,7 @@ echo "-> install node and npm"
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y gcc g++ make
+
+echo "---------------------------------------------------------"
+echo "-> install spaas"
+npm install && npm link
